2014c042606fe2223f45102f43061d6fde2b15b5
rabinpoudyal1995 <rabinpoudyal1995@gmail.com>
2018-09-11 15:52:49 +0545

    Initial commit

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 11 15:44:00 2018
+
+@author: rabin
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('Data.csv', delimiter=",")
+print(data.head())
+
